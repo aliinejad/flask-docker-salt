50fab15b15f792da48ce0b97dcee983da96ab41c
mdmadoff <80030346+mdmadoff@users.noreply.github.com>
2021-03-04 11:23:09 +0330

    create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__, static_folder='.', static_url_path='')
+
+@app.route("/")
+def root():
+    return 'Hello World!'
+
+@app.route('/sum',methods=['POST'])
+def asgher():
+  data=request.get_json()
+
+  result = data["number1"] + data["number2"]
+  return jsonify({"result": result})  
+  
+  
+  
+# start listening
+if __name__ == "__main__":
+    app.run(debug=True, port='8000', host='0.0.0.0')
